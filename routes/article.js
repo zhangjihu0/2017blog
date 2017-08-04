a2574fbc68d0926448db58f07d71b8f5da383042
V.Jihu.Zhang 張冀虎 <V.Jihu.Zhang@delta.corp>
2017-08-04 17:43:12 +0800

    up

--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let { Article } = require('../model')
 router.get('/add',function(req,res){
-    res.render('article/add',{title:'文章发表'})
+    res.render('article/add',{title:'文章发表',article:{}});
 });
 router.post('/add',function(req,res){
     let article = req.body;
@@ -32,5 +32,29 @@ router.get('/detail/:_id',function(req,res){
         }
     })
 });
+router.get('/update/:_id',function(req,res){
+    let _id = req.params._id;
+    Article.findById(_id,function(err,article){
+        //增加和更新可以复用一个模板
+       console.log(article);
+       res.render('article/add',{title:'更新文章',article});
+    });
+
+})
+router.post('/update/:_id',function(req,res){
+    let _id = req.params._id;
+    let article = req.body;
+    Article.update({_id},req.body,function(err,result){
+        if(err){
+            req.flash('error',err);
+            res.redirect('back');
+        }else{
+            req.flash('success','文章更新传成功');
+            res.redirect('/article/detail/'+_id);
+        }
+    });
+    
+
+})
 //module model
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@ let express = require('express');
 //调用Router方法可以得到一个路由中间件实例
 let { Article } = require('../model')
 let router = express.Router();
-
 router.get('/',function(req,res){
     //populate('user')将ref主键由原来的objectID转化为真实对象，
-    Article.find().populate('user').exec(function(err,articles){
+    let { keyword } = req.query;
+    let query ={};
+    if(keyword){
+      // query.title = new RegExp(keyword);// /b/
+      //特殊写法正文或正则符合要求就好；
+       query['$or'] = [{
+        title:new RegExp(keyword)   
+       },{content:new RegExp(keyword)}]
+    }
+    Article.find(query).populate('user').exec(function(err,articles){
          res.render('index',{title:'首页',articles})
-    })
+    });
     // Article.find({},function(err,articles){
     //     console.log(articles)
     //     res.render('index',{title:'首页',articles})
